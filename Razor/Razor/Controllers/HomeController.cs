97ed4cd2f3f3452641143d26de888c3d07781d92
Дмитрий Евреинов <d.evreinov@outlook.com>
2023-11-11 22:42:37 +0300

    Работа с Razor

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            //Product myProduct = new Product
+            //{
+            //    ProductId = 1,
+            //    Name = "Kayak",
+            //    Description = "A boat for one person",
+            //    Category = "Watersports",
+            //    Price = 275M,
+            //};
+            //ViewBag.StockLevel = 2;
+            //return View(myProduct);
+
+            Product[] array =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M},
+            };
+            return View(array);
+        }
+    }
+}
