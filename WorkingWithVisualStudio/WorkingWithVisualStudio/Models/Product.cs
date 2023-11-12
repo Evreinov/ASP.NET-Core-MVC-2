b0753937260fe15818528692002061bcf48ee6d1
Дмитрий Евреинов <d.evreinov@outlook.com>
2023-11-12 13:45:40 +0300

    Работа с Visual Studio

---
+++ WorkingWithVisualStudio/WorkingWithVisualStudio/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkingWithVisualStudio.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ WorkingWithVisualStudio/WorkingWithVisualStudio/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseMvcWithDefaultRoute();
+
+app.UseStaticFiles();
+
+app.Run();
